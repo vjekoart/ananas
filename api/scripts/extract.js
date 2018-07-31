d3c9f7e3093680831df3ce1541f43e19e0ddd071
Vjekoslav Artic <vjekoslav.artic@uniqcast.com>
2018-07-31 20:16:06 +0200

    [API] Finished extract.js script and filled dictionary with 5000 most common words.

--- api/scripts/extract.js
+++ api/scripts/extract.js
@@ -1,7 +1,33 @@
+const fs = require('fs')
 const jsdom = require('jsdom')
+
 const { JSDOM } = jsdom
 
 JSDOM.fromFile('./5000-most-common-words.html').then(dom => {
-    const target = dom.window.document.querySelector('[data-target]').innerHTML
-    console.log(target)
+    const target = dom.window.document.querySelector('[data-target]')
+    const results = []
+
+    const rows = target.querySelectorAll('tr')
+
+    /* Extract words from rows */
+    for (const row of rows) {
+        const cells = row.querySelectorAll('td')
+        const position = parseInt(cells[0].innerHTML, 10)
+
+        if (!isNaN(position)) {
+            const word = cells[1].innerHTML.replace('&nbsp;&nbsp;&nbsp;', '')
+
+            results.push(word)
+        }
+    }
+
+    /* Save array of words to dictionary */
+    fs.writeFile('../lib/dictionary.json', JSON.stringify({
+        words: results
+    }), error => {
+        if (error)
+            throw error
+
+        console.log('Array of words saved to lib/dictionary.json file!')
+    })
 })
