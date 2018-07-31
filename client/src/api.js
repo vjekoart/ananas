49e19a7c11c677923ee63d897c7a1f76cf55fa1b
Vjekoslav Artic <vjekoslav.artic@uniqcast.com>
2018-08-01 00:10:46 +0200

    [Client] Added try/catch block to api.js service.

--- client/src/api.js
+++ client/src/api.js
@@ -1,18 +1,22 @@
 import configuration from './configuration';
 
 async function convertNumbers(number) {
-  const rawResponse = await fetch(configuration.api + 'convert', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ data: number })
-  });
+  try {
+    const rawResponse = await fetch(configuration.api + 'convert', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ data: number })
+    });
 
-  const content = await rawResponse.json();
+    const content = await rawResponse.json();
 
-  return content.data ? content.data : null;
+    return content.data ? content.data : null;
+  } catch (error) {
+    return null;
+  }
 }
 
 export default convertNumbers;
