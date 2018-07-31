6ba5df91165904e4e44cced006d204e5966ce889
Vjekoslav Artic <vjekoslav.artic@uniqcast.com>
2018-07-31 23:49:43 +0200

    [API] Added CORS support.
    Signed-off-by: Vjekoslav Artic <vjekoslav.artic@uniqcast.com>

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const lib = require('./lib')
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
 
 /**
  * Endpoint: POST /convert
